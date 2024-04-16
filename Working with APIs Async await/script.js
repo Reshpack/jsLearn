d287c404c86d1aee0ac97f957cb24d96c0afe142
Suresh Packiry <sureshpackiry73@gmail.com>
2024-04-16 16:01:39 +1000

    learning async and await

---
+++ Working with APIs Async await/script.js
@@ -0,0 +1,25 @@
+// fetch('url', {optional, post req instead of git request})
+
+// Fetch always succeeds even if theres an error
+
+fetch('https://reqres.in/api/users', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        name: 'User 1'
+    })
+})
+    .then(res => {
+        if (res.ok) {
+            console.log('SUCCESS')
+        } else {
+            console.log('NOT SUCCESSFUL')
+        }
+        res.json()
+    })
+    .then(data => console.log(data))
+    .catch(error => console.log('ERROR'))
+
+    // within 200 - 299 is ok
