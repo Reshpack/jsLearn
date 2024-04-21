0f1455f4240208097f6f877bde57ae5eda70f425
Suresh Packiry <sureshpackiry73@gmail.com>
2024-04-22 01:08:46 +1000

    working on time efficiency

---
+++ Time complexity/Efficiencybasics.js
@@ -0,0 +1,12 @@
+function oddNumbersLessThanTen() {
+    let currentNumber = 1;
+  
+    while (currentNumber < 10) {
+      if (currentNumber % 2 !== 0) {
+        console.log(currentNumber);
+      }
+  
+      currentNumber += 1;
+    }
+  }
+
